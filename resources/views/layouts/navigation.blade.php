c92fc8707da154ae05f2d0c36c8e43a31ed6d80c
MaxPowerEdain <56118011+MaxPowerEdain@users.noreply.github.com>
2020-12-22 16:49:05 +0100

    some scafolding done

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,23 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('/')" :active="request()->routeIs('/')">
+                        Home
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Dashboard
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('events')" :active="request()->routeIs('events')">
+                        All events
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('user')" :active="request()->routeIs('user')">
+                        User profile
                     </x-nav-link>
                 </div>
             </div>
@@ -23,7 +38,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>Welcome {{ Auth::user()->name }}!</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->name('welcome');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/events', function () {
+    return view('events');
+})->name('events');
+
+Route::get('/user', function () {
+    return view('user');
+})->name('user');
+
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
 
 require __DIR__.'/auth.php';
