0ab90e664cedc47a57241f06e8eee8ddb368733c
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-22 17:02:09 +0000

    Update Events: add update route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::put('update/{event}', [\App\Http\Controllers\EventController::class , 'update'])->name('update')->middleware('auth');
