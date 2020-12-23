b8bea1e7873a1c438cf7a734bf01314e94f22560
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-23 08:48:57 +0000

    Unenroll from Event: add route

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,4 @@ Route::put('update/{event}', [\App\Http\Controllers\EventController::class , 'up
 Route::get('edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('auth');
 Route::delete('delete/{id}', [\App\Http\Controllers\EventController::class , 'destroy'])->name('delete')->middleware('auth');
 Route::post('enroll/{id}', [\App\Http\Controllers\UserController::class, 'enroll'])->name('enroll')->middleware('auth');
+Route::post('unenroll/{id}', [\App\Http\Controllers\UserController::class, 'unenroll'])->name('unenroll')->middleware('auth');
