44432d6c3989f3855bd966cb699ea8afd039a6cc
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-24 12:57:48 +0000

    DeleteEvent: added event in url before delete method

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ require __DIR__.'/auth.php';
 
 Route::put('event/update/{event}', [\App\Http\Controllers\EventController::class , 'update'])->name('update')->middleware('auth');
 Route::get('event/edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('auth');
-Route::delete('delete/{id}', [\App\Http\Controllers\EventController::class , 'destroy'])->name('delete')->middleware('auth');
+Route::delete('event/delete/{id}', [\App\Http\Controllers\EventController::class , 'destroy'])->name('delete')->middleware('auth');
 Route::post('enroll/{id}', [\App\Http\Controllers\UserController::class, 'enroll'])->name('enroll')->middleware('auth');
 Route::post('unenroll/{id}', [\App\Http\Controllers\UserController::class, 'unenroll'])->name('unenroll')->middleware('auth');
