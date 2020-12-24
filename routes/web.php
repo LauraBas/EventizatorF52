431ea8c67d1d23d31fa27f96e1d1a2fb298dc27e
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-24 13:16:37 +0000

    Admin Middleware: added admin middleware in edit event route

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/user', function () {
 require __DIR__.'/auth.php';
 
 Route::put('event/update/{event}', [\App\Http\Controllers\EventController::class , 'update'])->name('update')->middleware('auth');
-Route::get('event/edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('auth');
+Route::get('event/edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('admin');
 Route::delete('event/delete/{id}', [\App\Http\Controllers\EventController::class , 'destroy'])->name('delete')->middleware('admin');
 Route::post('enroll/{id}', [\App\Http\Controllers\UserController::class, 'enroll'])->name('enroll')->middleware('auth');
 Route::post('unenroll/{id}', [\App\Http\Controllers\UserController::class, 'unenroll'])->name('unenroll')->middleware('auth');
