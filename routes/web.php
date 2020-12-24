fed9205f430649ed1c46e368ddb71d2730fcd250
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-24 12:49:51 +0000

    Update Event: add event in route before method

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::get('/user', function () {
 
 require __DIR__.'/auth.php';
 
-Route::put('update/{event}', [\App\Http\Controllers\EventController::class , 'update'])->name('update')->middleware('auth');
-Route::get('edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('auth');
+Route::put('event/update/{event}', [\App\Http\Controllers\EventController::class , 'update'])->name('update')->middleware('auth');
+Route::get('event/edit/{id}',[\App\Http\Controllers\EventController::class , 'edit'])->name('edit')->middleware('auth');
 Route::delete('delete/{id}', [\App\Http\Controllers\EventController::class , 'destroy'])->name('delete')->middleware('auth');
 Route::post('enroll/{id}', [\App\Http\Controllers\UserController::class, 'enroll'])->name('enroll')->middleware('auth');
 Route::post('unenroll/{id}', [\App\Http\Controllers\UserController::class, 'unenroll'])->name('unenroll')->middleware('auth');
