7efb99553366d2815f25fe7e4a6c3d3b8efc92b1
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-28 11:25:36 +0000

    Admin Middleware: Added admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Event::factory(10)->create();
+       User::factory(10)->create();
+       Event::factory(10)->create();
+       User::factory(1)->create(['name'=>'admin',
+            'email'=>'admin@admin',
+            'password' => Hash::make("12345678"),
+            'isAdmin'=> 1]);
     }
 }
