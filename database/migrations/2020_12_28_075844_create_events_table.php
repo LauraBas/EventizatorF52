b08d7e3414e2c0063a24f8fd7c3c489bbe65365a
Laura Bassani <bassanilaurarita@gmail.com>
2020-12-29 19:48:04 +0000

    Migrations: Added events and event-user migration

---
+++ database/migrations/2020_12_28_075844_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('date');
+            $table->string('time');
+            $table->string('description');
+            $table->integer('capacity');
+            $table->integer('participants');
+            $table->string('requirements');
+            $table->string('image');
+            $table->boolean('isHighlighted')->default(false);
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
