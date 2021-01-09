5b11fbbf3cc1fe5d526d3211557903e8869495da
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-09 08:47:04 +0000

    Events: added local images in seder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        User::factory(10)->create();
-       Event::factory(10)->create(['image'=>'https://tinyurl.com/y5wshz4s']);
-       Event::factory(5)->create(['isHighlighted'=> 1, 'image'=>'https://tinyurl.com/y3csscxh', 'date' => '2026-08-22']);
+       Event::factory(5)->create(['image'=>'/winter.jpg']);
+       Event::factory(5)->create(['image'=>'/navidad.jpeg', 'date' => '2022-08-22']);
+       Event::factory(5)->create(['isHighlighted'=> 1, 'image'=>'/boreal.jpeg', 'date' => '2026-08-22']);
        User::factory(1)->create(['name'=>'admin',
             'email'=>'admin@admin',
             'password' => Hash::make("12345678"),
             'isAdmin'=> 1]);
     }
 }
+
