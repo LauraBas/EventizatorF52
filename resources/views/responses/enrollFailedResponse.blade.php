489067e67be9551df43a191bba521d179d27bd1e
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-09 09:04:01 +0000

    Responses: added style

--- resources/views/responses/enrollFailedResponse.blade.php
+++ resources/views/responses/enrollFailedResponse.blade.php
@@ -1,5 +1,14 @@
 <x-app-layout>
-    <h3>{{ $message }}</h3>
-    <a href="/user">Back To Profile</a>
+    
+    <div class="p-2">
+        <div class="p-4">
+                <h3 class="lg:text-2xl font-bold text-yellow-600 text-center">{{ $message }}</h3>
+        </div>
+        <div class="p-2 flex justify-center">
+            <button class="h-10 px-6 text-base text-yellow-600 transition-colors duration-150 bg-white rounded-lg focus:shadow-outline hover:bg-yellow-200">
+                <a href="/user">Back To Profile</a>
+            </button>
+        </div>
+    </div>
 
 </x-app-layout>
