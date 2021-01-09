6741394f2873ad1911181df2370d1289f44f737c
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-09 09:15:56 +0000

    PArticipants In event: added style

--- resources/views/participantsInEvent.blade.php
+++ resources/views/participantsInEvent.blade.php
@@ -1,13 +1,10 @@
 <x-app-layout>
-
-    <div class="md:container md:mx-auto p-8 flex justify-center text-6l">
-       Participants in event {{$event->title}}
+    <div class="md:container md:mx-auto p-8 flex justify-center text-yellow-600 pt-4 lg:text-2xl">
+       Participants in event: {{$event->title}}
     </div>          
-    @foreach($participantsInEvent as $participant)
-        
-        <li class=" pl-3 list-decimal">{{ $participant }}</li>
-        
-    @endforeach
-        
-   
+    @foreach($participantsInEvent as $participant)       
+        <li class=" pl-3 list-decimal lg:text-2xl font-bold text-yellow-600">
+            {{ $participant }}
+        </li>       
+    @endforeach  
 </x-app-layout>
