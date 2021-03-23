476c7ff21344c2fb385157d1e59e226272601b51
Laura Bassani <bassanilaurarita@gmail.com>
2021-03-23 13:04:09 +0100

    Mail: comment email to prevet heroku errors

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
 
         if ($user->enrollToEvent($eventId))
         {
-            Mail::to($user->email)->send(new EnrollEventEmail($event));
+            //Mail::to($user->email)->send(new EnrollEventEmail($event));
             return view('responses.enrollResponse', ["message" => "You're enroll in the event " . $event->title]);
         }
         return view('responses.enrollFailedResponse', ["message" => "You're already enroll in the event " . $event->title]);
@@ -31,7 +31,7 @@ class UserController extends Controller
         $user = User::find($userId);
         $event = Event::find($eventId);
         $user->unenrollFromEvent($eventId);
-        Mail::to($user->email)->send(new UnenrollEventEmail($event));
+       // Mail::to($user->email)->send(new UnenrollEventEmail($event));
         return redirect('user');
     }
 
